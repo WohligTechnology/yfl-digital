6e579449e7ca97be64101457c502a9613e024f2b
Ruchiwohlig <ruchiwohlig@gmail.com>
2016-10-03 15:05:18 +0530

    second form done

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ var firstapp = angular.module('firstapp', [
     'angulartics.google.analytics'
 ]);
 
-firstapp.config(function($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider) {
+firstapp.config(function ($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider) {
     // for http request with session
     $httpProvider.defaults.withCredentials = true;
     $stateProvider
@@ -18,26 +18,33 @@ firstapp.config(function($stateProvider, $urlRouterProvider, $httpProvider, $loc
             templateUrl: "views/template.html",
             controller: 'HomeCtrl'
         })
-        .state('form', {
-            url: "/form",
-            templateUrl: "views/template.html",
-            controller: 'FormCtrl'
-        });
+
+    .state('branding', {
+        url: "/branding",
+        templateUrl: "views/template.html",
+        controller: 'BrandCtrl'
+    })
+
+    .state('form', {
+        url: "/form",
+        templateUrl: "views/template.html",
+        controller: 'FormCtrl'
+    });
     $urlRouterProvider.otherwise("/");
     $locationProvider.html5Mode(isproduction);
 });
 
 
-firstapp.directive('img', function($compile, $parse) {
+firstapp.directive('img', function ($compile, $parse) {
     return {
         restrict: 'E',
         replace: false,
-        link: function($scope, element, attrs) {
+        link: function ($scope, element, attrs) {
             var $element = $(element);
             if (!attrs.noloading) {
                 $element.after("<img src='img/loading.gif' class='loading' />");
                 var $loading = $element.next(".loading");
-                $element.load(function() {
+                $element.load(function () {
                     $loading.remove();
                     $(this).addClass("doneLoading");
                 });
@@ -48,11 +55,11 @@ firstapp.directive('img', function($compile, $parse) {
     };
 });
 
-firstapp.directive('fancybox', function($document) {
+firstapp.directive('fancybox', function ($document) {
     return {
         restrict: 'EA',
         replace: false,
-        link: function(scope, element, attr) {
+        link: function (scope, element, attr) {
             var $element = $(element);
             var target;
             if (attr.rel) {
@@ -74,11 +81,11 @@ firstapp.directive('fancybox', function($document) {
     };
 });
 
-firstapp.directive('autoHeight', function($compile, $parse) {
+firstapp.directive('autoHeight', function ($compile, $parse) {
     return {
         restrict: 'EA',
         replace: false,
-        link: function($scope, element, attrs) {
+        link: function ($scope, element, attrs) {
             var $element = $(element);
             var windowHeight = $(window).height();
             $element.css("min-height", windowHeight);
@@ -86,7 +93,7 @@ firstapp.directive('autoHeight', function($compile, $parse) {
     };
 });
 
-firstapp.config(function($translateProvider) {
+firstapp.config(function ($translateProvider) {
     $translateProvider.translations('en', LanguageEnglish);
     $translateProvider.translations('hi', LanguageHindi);
     $translateProvider.preferredLanguage('en');
